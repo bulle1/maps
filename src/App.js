6cb8be6bff593dc89540f6454064c16bf11529ce
[Gautam] <[mailforvijay3@gmail.com]>
2020-08-28 22:58:44 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+import { Icon } from "leaflet";
+import * as carData from "./data/car-data.json";
 
 function App() {
+
+  const [activeCar , setActiveCar] = useState(null);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Map center = {[25.427210,86.132370]} zoom = {12}>
+    <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+   />
+    {carData.objects.map(car => (
+    <Marker
+      key = {car.name}
+      position = {[car.lat,car.long]}
+      onclick = {() => {
+        setActiveCar(car);
+      }}
+    />
+    ))}
+
+    {activeCar && 
+    <Popup 
+    position = {[activeCar.lat,activeCar.long]}
+    onClose={() => {
+      setActiveCar(null);
+    }}
+    >
+    <div>
+      <h2>
+        {activeCar.name}
+      </h2>
     </div>
+    </Popup>
+    
+    
+    }
+   </Map>
   );
 }
 
